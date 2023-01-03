9e03ec83be8971a173e8fe2fd55481bc5ec5e512
mmtmn <thiagomnobrega@gmail.com>
2023-01-03 10:45:53 -0300

    defaulted back to matplotlib

---
+++ main.py
@@ -0,0 +1,15 @@
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+x = np.random.standard_normal(100)
+y = np.random.standard_normal(100)
+z = np.random.standard_normal(100)
+c = np.random.standard_normal(100)
+
+img = ax.scatter(x, y, z, c=c, cmap=plt.hot())
+fig.colorbar(img)
+plt.show()
